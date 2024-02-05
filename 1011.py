4c05ba0fd1d0c8f9c93be232f9045641c45473ca
artneto <arthur_neto@icloud.com>
2024-02-05 10:05:37 +0000

    pratice

---
+++ 1011.py
@@ -0,0 +1,9 @@
+#Faça um programa que 
+#calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio (R)
+r = float(input())
+pi = 3.14159
+
+
+VOLUME = 4/3 * pi * r**3
+
+print(f'VOLUME = {VOLUME:.3f}')
