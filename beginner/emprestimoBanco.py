ced85a00dea582596b1ba40ed09fdfe769572428
artneto <arthur_neto@icloud.com>
2024-02-13 15:48:18 +0000

    pratice

--- beginner/emprestimoBanco.py
+++ beginner/emprestimoBanco.py
@@ -1,12 +1,16 @@
 import time
-cliente01 = int(input('Quando foi a abertura de sua conta em anos: '))
+from datetime import datetime
+
+ano_atual = datetime.now().year
+
+cliente01 = int(input('Qual ano foi a abertura de sua conta: '))
 valor_deposito = float(input('Qual o valor do seu deposito: '))
 
-sum01 = 2024 - cliente01
+sum01 = ano_atual - cliente01
 
 if sum01 >= 5 or valor_deposito >= 5000:
     print('processando')
-    time.sleep(1)
+    time.sleep(0.5)
     print('Aprovado')
 else:
     print('Negado, voce ainda nao cumpriu os requirements')
---
+++ beginner/marioKart.py
@@ -0,0 +1,6 @@
+import random
+
+amigos = ['lucas', 'joao', 'Paulo', 'Ana', 'Pedro']
+
+random.shuffle(amigos)
+print(f'Primeiro lugar foi {amigos[0]}, Quarto lugar {amigos[3]}, Quinto lugar {amigos[4]}')
