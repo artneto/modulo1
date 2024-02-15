146a25e4ceaeba8fefb437a5de01fe9fae2809f6
artneto <arthur_neto@icloud.com>
2024-02-15 22:48:02 +0000

    practice

--- beginner/jokepo.py
+++ beginner/jokepo.py
@@ -1,25 +1,38 @@
 import random
+
 #Pedra perde para papel
 #papel perde para tesoura
 #tesoura perde para a pedra
-userChoice = input('Choose between\n'
-' Paper, rock, scissor: ')
 
-usedObject = ['rock', 'Paper', 'scissor']
-computadorChoice = random.choice(usedObject)
+while True:
+    userChoice = str(input('Choose between\n'
+    'Paper, rock, scissor: ')).lower()
 
-print(f'{usedObject} choice and {computadorChoice} choice')
+    usedObject = ['rock', 'Paper', 'scissor']
+    computadorChoice = random.choice(usedObject)
 
-if userChoice == computadorChoice:
-    print(f'Player choice {userChoice} and computer choise is {computadorChoice}, this is a tie')
-elif userChoice == 'Paper':
-    if computadorChoice == 'rock':
-        print(f'Paper wins!')
-elif userChoice == 'rock':
-    if computadorChoice == 'scissor':
-        print(f'rock wins!')
-elif userChoice == 'scissor':
-    if computadorChoice == 'paper':
-        print(f'scissor wins!')
-else:
-    print('option not found, try again')
+    print(f'Player choice: {userChoice} and computer choice: {computadorChoice}')
+
+    if userChoice == computadorChoice:
+            print(f'Player choice {userChoice} and computer choise is {computadorChoice}, this is a tie')
+    elif userChoice == 'Paper':
+        if computadorChoice == 'rock':
+            print(f'Paper wins!')
+        else:
+            print('Scissor wins')
+    elif userChoice == 'rock':
+        if computadorChoice == 'scissor':
+            print(f'rock wins!')
+        else:
+            print('Paper wins')
+    elif userChoice == 'scissor':
+        if computadorChoice == 'paper':
+            print(f'scissor wins!')
+        else:
+            print('rock wins!')
+    else:
+        print('option not found, try again')
+
+    play_again = str(input('Dou you want play again (Y/N): ')).lower()
+    if play_again != 'Y':
+        break
