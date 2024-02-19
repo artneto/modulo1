4194ab5957da7d3962f57368c2358b6a0781d05b
artneto <arthur_neto@icloud.com>
2024-02-19 16:31:30 +0000

    practice

--- beginner/challengeFor.py
+++ beginner/challengeFor.py
@@ -1,3 +1,20 @@
-for i in range(1,1002,2):
+# Create a programme that print from 1 to 1000 counting 2:
+for i in range(1,1001,2):
     print(i)
 
+##Cereate a programme that print from 0 to 100 and print barril for every multiple of 3
+
+for i in range(101):
+    if i % 3 ==0:
+        print('barril', end=" ")
+    else:
+        print(i, end= " ")
+
+# Check if the number is even or odd: 
+
+player01 = int(input('Type a number: '))
+
+if player01 % 2 == 0:
+    print(f'{player01} number is even')
+else:
+    print(f'{player01} number is odd')
