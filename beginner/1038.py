68ea1b3747b159ed07c8c9b118a8ce0630ddd3f5
artneto <arthur_neto@icloud.com>
2024-02-20 16:44:36 +0000

    pratice

--- beginner/1038.py
+++ beginner/1038.py
@@ -1,13 +1,21 @@
 #Based on the table below, write a program 
 # that reads the code of an item and the quantity of this item. Next, calculate and show the amount of the bill payable.
-menu = [
-    {'codigo': 1, 'especificacao': 'cachorro quente', 'preco': 4.00},
-    {'codigo': 2, 'especificacao': 'X-salada', 'preco': 4.50},
-    {'codigo': 3, 'especificacao': 'X-bacon', 'preco': 5.00},
-    {'codigo': 4, 'especificacao': 'torrada simples', 'preco': 2.00},
-    {'codigo': 5, 'especificacao': 'refrigerante', 'preco': 1.00}
-]
+# Definição da tabela de itens
+menu = {
+    1: {'especificacao': 'cachorro quente', 'preco': 4.00},
+    2: {'especificacao': 'X-salada', 'preco': 4.50},
+    3: {'especificacao': 'X-bacon', 'preco': 5.00},
+    4: {'especificacao': 'torrada simples', 'preco': 2.00},
+    5: {'especificacao': 'refrigerante', 'preco': 1.00}
+}
 
-pedido_01 = int(input('Type the code number:  '))
-pedidoQty = int(input('Type the quantity: '))
+# Entrada dos valores do código do item e quantidade
+codigo, quantidade = map(int, input().split())
 
+# Verificação se o código está presente na tabela de itens
+if codigo in menu:
+    # Cálculo do total a pagar
+    total_pagar = menu[codigo]['preco'] * quantidade
+    print(f'Total: R$ {total_pagar:.2f}')
+else:
+    print('Código de item inválido')
