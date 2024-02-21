36e88b18e3fe6f63614365395262cd428935ae5b
artneto <arthur_neto@icloud.com>
2024-02-21 21:40:48 +0000

    practice

--- beginner/1047.py
+++ beginner/1047.py
@@ -1,5 +1,6 @@
 #Read the start time, start minute, end time and end minute of a game. 
 # Then calculate the duration of the game.
 #Note: The game has a minimum duration of one (1) minute and a maximum duration of 24 hours.
+import time
 start_time,start_minute, end_time, end_minute  = map(int, input().split())
 
