9ad07eee325ea1c82feadafb7e97956912179d5e
artneto <arthur_neto@icloud.com>
2024-02-25 14:16:39 +0000

    practice

--- beginner/1050.py
+++ beginner/1050.py
@@ -1,15 +1,20 @@
 #Read an integer that represents a long-distance area code.
 # Then, inform which city the area code belongs to, considering the table below:
-DDD = (61 , 71, 11, 21, 32, 19, 27, 31)
-Destination = ('Brasilia', 'Salvador', 'SP', 'RJ', 'Juiz de fora', 'Campinas', 'Vitoria', 'BH')
-
+ddd_city = {
+    61: 'Brasilia',
+    71: 'Salvador',
+    11: 'Sao Paulo',
+    21: 'Rio de Janeiro',
+    32: 'Juiz de Fora',
+    19: 'Campinas',
+    27: 'Vitoria',
+    31: 'Belo Horizonte'
+}
 
 numero = int(input())
 
-if numero in DDD:
-    index = DDD.index(numero)
-    cidade = Destination[index]
-    print(cidade)
+if numero in ddd_city:
+   print(ddd_city[numero])
 else:
     print('DDD nao cadastrado')
 
