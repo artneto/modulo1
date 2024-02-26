759b601b2d7653ab88c703d45f787efcbdd7c6c8
artneto <arthur_neto@icloud.com>
2024-02-26 19:05:09 +0000

    pracitce

--- beginner/1066.py
+++ beginner/1066.py
@@ -1,16 +1,31 @@
 # Read 5 Integer values. Next, show how many values entered were even, how many values entered were
 #  odd, how many values entered were positive and how many values entered were negative.
-num1 = int(input())
-num2 = int(input())
-num3 = int(input())
-num4 = int(input())
-num5 = int(input())
+numeros=[]
 
-for i in range(num1, num2, num3, num4, num5):
-    if i % 2 == 0:
+pares = 0
+impar = 0
+positivo = 0
+negativo = 0
 
-print(f'{i} valor(es) par(es)')
-print(f'{2} valor(es) impar(es)')
-print(f'{1} valor(es) positivo(s)')
-print(f'{3} valor(es) negativo(s)')
+for i in range(5):
+    user_collection = int(input())
+    numeros.append(user_collection)
+    if user_collection % 2 == 0:
+        pares += 1
+
+    if user_collection % 2 !=0:
+        impar += 1
+    
+    if user_collection > 0:
+        positivo += 1
+
+    elif user_collection < 0:
+        negativo += 1
+
+print(f'{pares} valor(es) par(es)')
+print(f'{impar} valor(es) impar(es)')
+print(f'{positivo} valor(es) positivo(s)')
+print(f'{negativo} valor(es) negativo(s)')
+    
+                
 
