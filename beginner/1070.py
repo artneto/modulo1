9383c8bca2e2bbb7604bf740cbd4733052e75371
artneto <arthur_neto@icloud.com>
2024-02-29 09:14:23 +0000

    practice

--- beginner/1070.py
+++ beginner/1070.py
@@ -1,2 +1,8 @@
 #Read an integer value X. Then present the 6 consecutive odd values starting from X,
 #  one value per line, including X if applicable.
+
+X = int(input())
+
+for i in range(X, X + 12):
+    if i % 2 != 0:
+        print(i)
