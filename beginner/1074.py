299f928cb607d631cfc3604dae1a24c14e2a7f76
artneto <arthur_neto@icloud.com>
2024-02-29 11:16:21 +0000

    practice

--- beginner/1074.py
+++ beginner/1074.py
@@ -3,22 +3,19 @@
 # odd (ODD), positive (POSITIVE) or negative (NEGATIVE).
 # If the value is equal to zero (0), although the correct description is (EVEN NULL), 
 # as by definition zero is even, your program should only print NULL.
-
 N = int(input())
 
-for c in range(N):
-    valor = int(input(' '))
+for _ in range(N):
+    valor = int(input())
     if valor == 0:
         print('NULL')
-    if valor > 0:
-        print('POSITIVE')
-        if valor % 2 != 0:
-            print('ODD POSITIVE')
-    if valor < 0:
-        print('NEGATIVE')
-        if valor % 2 != 0:
-            print('ODD NEGATIVE')
     elif valor % 2 == 0:
-        print('EVEN')
+        if valor > 0:
+            print('EVEN POSITIVE')
+        else:
+            print('EVEN NEGATIVE')
     else:
-        print('ODD')
+        if valor > 0:
+            print('ODD POSITIVE')
+        else:
+            print('ODD NEGATIVE')
