1a913c80841948389eda7c4cdfcbd19f105b0276
artneto <arthur_neto@icloud.com>
2024-03-07 19:31:20 +0000

    practice

---
+++ beginner/1173.py
@@ -0,0 +1,6 @@
+#Read a value and write a program that places the read value in the first position of a vector N[10].
+#  In each subsequent position, place twice the value of the previous position. 
+# For example, if the read value is 1, the vector values must be 1,2,4,8 and so on.
+#  Show the vector next.
+#For each vector position, write "N[i] = X", where i is the vector position and X is 
+# the value stored at position i. The first number of vector N (N[0]) will receive the value of V.
