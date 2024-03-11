52761965c382861e07327ed89c4f332e37821f21
artneto <arthur_neto@icloud.com>
2024-03-11 17:39:01 +0000

    pracitce

--- beginner/1173.py
+++ beginner/1173.py
@@ -4,3 +4,12 @@
 #  Show the vector next.
 #For each vector position, write "N[i] = X", where i is the vector position and X is 
 # the value stored at position i. The first number of vector N (N[0]) will receive the value of V.
+v = int(input())
+N =[0]*10
+N[0] = v   
+
+for i in range(1,10):
+    N[i] = N[i - 1]* 2
+    
+for i in range(10):
+    print(f'N[{i}] = {N[i]}')
---
+++ beginner/1174.py
@@ -0,0 +1,4 @@
+#Write a program that reads a vector A[100]. At the end, show all positions in the vector that store a value less than or equal to 10 and the value stored in each of the positions.
+#The input contains 100 values, which can be integers, reals, positive or negative.
+#For each vector value less than or equal to 10, write "A[i] = x", where i is the position of the vector and x is the value stored at the position, with one place after the decimal point.
+
