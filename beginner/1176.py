159cc0811ffcdfc694233c7595cea4779f0189f8
artneto <arthur_neto@icloud.com>
2024-03-17 21:18:29 +0000

    practice

---
+++ beginner/1176.py
@@ -0,0 +1,4 @@
+#Write a program that reads a value and displays the Fibonacci number corresponding 
+# to this read value. Remember that the first 2 elements of the Fibonacci series are
+#  0 and 1 and each next term is the sum of the 2 before it. All Fibonacci values calculated
+#  in this problem must fit into an unsigned 64-bit integer.
