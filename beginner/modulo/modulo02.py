c88a596539d21f77a9bc685270d51dbaf440430f
artneto <arthur_neto@icloud.com>
2024-03-27 16:02:55 +0000

    practice

---
+++ beginner/modulo/modulo02.py
@@ -0,0 +1,7 @@
+#Create a program that receives the ten names that were drawn in the
+#lady bingo. Knowing that bingo had ten prizes, inform
+#Ms. Sueli, what was the first time she was drawn and how many times was her name
+#he appeared. Considering that the ladies named Eneide and Nair were not there to
+#receive the prize, the program also informs how many more draws should be
+#made to complete the ten prizes.
+
