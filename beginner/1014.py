a6197afa7d26efdba2fdd888328c31b0183a81a6
artneto <arthur_neto@icloud.com>
2024-03-27 14:23:21 +0000

    practice

---
+++ beginner/1014.py
@@ -0,0 +1,12 @@
+#Calculate the average consumption of a car
+# by providing the total distance traveled (in km) and the total fuel used (in liters).
+
+# X represent KM
+# Y represent total fuel spent, with a digit after the decimal point.
+
+X = int(input())
+Y = float(input())
+
+cosumo_total = X / Y
+
+print(f'{cosumo_total:.3f} km/l')
